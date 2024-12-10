d4a6ccee628e41196f500821443e4a36221cc4cd
Unknown_nymus18 <felixasante2005@gmail.com>
2024-12-10 20:03:55 +0000

    cart functionality

---
+++ pages/add_to_cart.dart
@@ -0,0 +1,19 @@
+import 'package:e_commerce/pages/cart_page.dart';
+import 'package:flutter/material.dart';
+
+class AddToCart extends StatefulWidget {
+  const AddToCart({super.key});
+
+  @override
+  State<AddToCart> createState() => _AddToCartState();
+}
+
+class _AddToCartState extends State<AddToCart> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: CartPage(),
+    );
+  }
+}
